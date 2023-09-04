de0ef4d7c0d047eb89af41c7a68fd9ecb8f01719
Dimitar Tonev <dimtoneff@gmail.com>
2023-09-05 00:12:53 +0200

    Added an automated check for the opencv library (cv2)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
 import shutil
 import folder_paths
-import os
+import os, sys, subprocess
 
+try:
+   import cv2
+except ImportError:
+   print(f"## PixelArtDetector: installing dependencies")
+   my_path = os.path.dirname(__file__)
+   requirements_path = os.path.join(my_path, "requirements.txt")
+   try:
+      subprocess.check_call([sys.executable, '-s', '-m', 'pip', 'install', '-r', requirements_path])
+   except:
+      try:
+         subprocess.check_call([sys.executable, '-s', '-m', 'pip', 'install', '-r', requirements_path, '--use-pep517'])
+      except:
+         print(f"## [ERROR] PixelArtDetector: Could not install the needed libraries.")
+   print(f"## PixelArtDetector: installing dependencies done.")
+    
 print("### Loading: PixelArtDetector")
 
 comfy_path = os.path.dirname(folder_paths.__file__)
